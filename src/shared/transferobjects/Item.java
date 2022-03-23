8082028e700cca290ce361300beb523b6c661bf9
Shmeat <miloamadsen@gmail.com>
2022-03-23 13:19:45 +0100

    Database connection fuldført

--- src/shared/transferobjects/Item.java
+++ src/shared/transferobjects/Item.java
@@ -4,7 +4,35 @@ public class Item {
 
 	private String title;
 	private String description;
-	private String[] tags;
+	private String tags;
 	private String itemId;
+	private double starterPrice;
 
+	public Item(String title, String description, String tags, double starterPrice)
+	{
+		this.title = title;
+		this.description = description;
+		this.tags = tags;
+		this.starterPrice = starterPrice;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public Double getStarterPrice() {
+		return starterPrice;
+	}
 }
