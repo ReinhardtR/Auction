0511a855b057de8ef81b5314a9d9724c9a3649c3
ymersod <simon.99@live.dk>
2022-05-11 14:08:08 +0200

    Update AuctionViewModel.java

--- src/client/views/auction/AuctionViewModel.java
+++ src/client/views/auction/AuctionViewModel.java
@@ -14,6 +14,7 @@ import java.beans.PropertyChangeListener;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.Temporal;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -22,6 +23,8 @@ public class AuctionViewModel implements PropertyChangeListener {
 	private final IntegerProperty currentHighestBid;
 	private final StringProperty timeLeft;
 	private final ObservableItem item;
+
+	private List<String> itemIdProperties;
 
 	public AuctionViewModel(ObservableItemListImpl observableItemList) {
 		itemText = new SimpleStringProperty();
