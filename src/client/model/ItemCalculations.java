93cc90aa9f26bcf87bbf1dfdc01e222a2b8e8841
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2022-05-11 11:21:35 +0200

    Update ItemCalculations.java

--- src/client/model/ItemCalculations.java
+++ src/client/model/ItemCalculations.java
@@ -2,9 +2,7 @@ package client.model;
 
 
 public class ItemCalculations {
-
 	public static boolean calculateNewOffer(ObservableItem item, int offer) {
 		return item.getOfferAmount() > offer;
 	}
-
 }
