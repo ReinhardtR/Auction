628707ecfb410b734d22e97b079fc2c9fe1e27d3
ymersod <simon.99@live.dk>
2022-05-11 09:32:20 +0200

    Update ObservableItemListImpl.java

--- src/client/model/ObservableItemListImpl.java
+++ src/client/model/ObservableItemListImpl.java
@@ -10,52 +10,48 @@ import java.beans.PropertyChangeSupport;
 import java.rmi.RemoteException;
 import java.util.HashMap;
 
-public class ObservableItemListImpl implements ObservableItemList {
+public class ObservableItemListImpl implements ObservableItemList, PropertyChangeListener, PropertyChangeSubject {
+
 	private final LocalClient client;
 	private final HashMap<String, ObservableItem> itemsForClient = new HashMap<>();
 
-	public class ObservableItemListImpl implements ObservableItemList, PropertyChangeListener, PropertyChangeSubject {
+	private final PropertyChangeSupport support;
 
-		private final LocalClient client;
-		private final HashMap<String, ObservableItem> itemsForClient = new HashMap<>();
-
-		private final PropertyChangeSupport support;
-
-		public ObservableItemListImpl(LocalClient client) {
-			this.client = client;
-			support = new PropertyChangeSupport(this);
-		}
-
-		@Override
-		public ObservableItem getItemForAuction(String itemID) {
-			ObservableItem observableItem = itemsForClient.get(itemID);
-
-			if (observableItem == null) {
-				try {
-					Item item = client.getItem(itemID);
-					observableItem = new ObservableItem(client, item);
-					observableItem.addListener(itemID, this);
-					itemsForClient.put(itemID, observableItem);
-					System.out.println(itemID + " was put in");
-				} catch (RemoteException e) {
-					e.printStackTrace();
-				}
-			}
-			return observableItem;
-		}
-
-		@Override
-		public void propertyChange(PropertyChangeEvent evt) {
-			support.firePropertyChange("model", null, evt.getNewValue());
-		}
-
-		@Override
-		public void addListener(String eventName, PropertyChangeListener listener) {
-			support.addPropertyChangeListener(eventName, listener);
-		}
-
-		@Override
-		public void removeListener(String eventName, PropertyChangeListener listener) {
-			support.removePropertyChangeListener(eventName, listener);
-		}
+	public ObservableItemListImpl(LocalClient client) {
+		this.client = client;
+		support = new PropertyChangeSupport(this);
 	}
+
+	@Override
+	public ObservableItem getItemForAuction(String itemID) {
+		ObservableItem observableItem = itemsForClient.get(itemID);
+
+		if (observableItem == null) {
+			try {
+				Item item = client.getItem(itemID);
+				observableItem = new ObservableItem(client, item);
+				observableItem.addListener(itemID, this);
+				itemsForClient.put(itemID, observableItem);
+				System.out.println(itemID + " was put in");
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
+		return observableItem;
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		support.firePropertyChange("model", null, evt.getNewValue());
+	}
+
+	@Override
+	public void addListener(String eventName, PropertyChangeListener listener) {
+		support.addPropertyChangeListener(eventName, listener);
+	}
+
+	@Override
+	public void removeListener(String eventName, PropertyChangeListener listener) {
+		support.removePropertyChangeListener(eventName, listener);
+	}
+}
