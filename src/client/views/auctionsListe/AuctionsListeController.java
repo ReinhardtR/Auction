761fe84be110fd4ad6196d04fa8210acfdf64a7e
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2022-05-11 15:01:16 +0200

    controller

--- src/client/views/auctionsListe/AuctionsListeController.java
+++ src/client/views/auctionsListe/AuctionsListeController.java
@@ -4,46 +4,33 @@ import client.core.ViewHandler;
 import client.core.ViewModelFactory;
 import client.model.ObservableItem;
 import client.views.ViewController;
-import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.TextFieldTableCell;
 
 public class AuctionsListeController implements ViewController {
 
 	@FXML
 	public TableView<ObservableItem> itemsTableView;
-	private ObservableList<ObservableItem> observableList;
-	private AuctionsListeViewModel auctionsListeViewModel;
 
 	public void init(ViewHandler viewHandler, ViewModelFactory viewModelFactory) {
-		this.auctionsListeViewModel = viewModelFactory.getAuctionsListeViewModel();
-
-		tableSetUp();
+		tableSetUp(viewModelFactory.getAuctionsListeViewModel().getObservableItemList());
 	}
 
 	public void getNewAuctionView() {
 		ViewHandler.getInstance().openAuctionView();
 	}
 
-	private void tableSetUp() {
-		Platform.runLater(() -> {
-			observableList = auctionsListeViewModel.getObservableItemList();
-			itemsTableView = new TableView<>();
+	private void tableSetUp(ObservableList<ObservableItem> observableList) {
+		System.out.println("Size: " + observableList.size());
+		itemsTableView = new TableView<>();
 
-			TableColumn<ObservableItem, String> idCol = new TableColumn<>("ID");
-			idCol.setCellFactory(TextFieldTableCell.forTableColumn());
-			idCol.setCellValueFactory((item) -> {
-				System.out.println("CELL FACTORY");
-				return new ReadOnlyStringWrapper(item.getValue().getItemID());
-			});
+		TableColumn<ObservableItem, String> idCol = new TableColumn<>("ID");
+		idCol.setCellValueFactory(item -> new ReadOnlyStringWrapper(item.getValue().getItemID()));
 
-			itemsTableView.getColumns().setAll(idCol);
-
-			itemsTableView.setItems(observableList);
-		});
+		itemsTableView.getColumns().add(idCol);
+		itemsTableView.getItems().addAll(observableList);
 	}
 }
