3f3e1dee4daaf14fa087aea70644568a65a2eb76
ymersod <simon.99@live.dk>
2022-05-13 12:15:00 +0200

    sdf
    sdf

--- test/ItemTest.java
+++ test/ItemTest.java
@@ -1,14 +1,24 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import server.model.item.Item;
+import server.model.item.ItemImpl;
+import server.model.item.SaleStrategy.AuctionStrategy;
 
-
+import java.rmi.RemoteException;
+import java.time.LocalDateTime;
 
 
 public class ItemTest {
-
-	
+	private Item testItem;
+	@BeforeEach
+	void createTestItem() throws RemoteException {
+			testItem = new ItemImpl("123", LocalDateTime.of(2022, 5, 16, 11, 20), new AuctionStrategy(0));
+	}
 
 
 	@Test
-	public void name() {
+	public void simpleIdTestTest() {
+		Assertions.assertTrue(testItem.getItemID());
 	}
 }
