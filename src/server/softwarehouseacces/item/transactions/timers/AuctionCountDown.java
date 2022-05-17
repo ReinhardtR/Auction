3fba80a2f7a8d5788f2acda2af51c14a7723dfd2
Shmeat <miloamadsen@gmail.com>
2022-05-17 11:33:10 +0200

    DEEAMF

--- src/server/softwarehouseacces/item/transactions/timers/AuctionCountDown.java
+++ src/server/softwarehouseacces/item/transactions/timers/AuctionCountDown.java
@@ -5,15 +5,16 @@ import java.beans.PropertyChangeSupport;
 import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.temporal.Temporal;
 
 public class AuctionCountDown implements Runnable {
 	private final String itemID;
-	private final Timestamp endTime;
-	private final LocalDateTime localTimeIn1Hour;
+	private final Temporal endTime;
+	private final Temporal localTimeIn1Hour;
 	private final PropertyChangeSupport support;
 
 
-	public AuctionCountDown(String itemID, Timestamp endTime, LocalDateTime localTimeIn1Hour, PropertyChangeListener listener) {
+	public AuctionCountDown(String itemID, Temporal endTime, LocalDateTime localTimeIn1Hour, PropertyChangeListener listener) {
 		this.itemID = itemID;
 		this.endTime = endTime;
 		this.localTimeIn1Hour = localTimeIn1Hour;
@@ -21,15 +22,14 @@ public class AuctionCountDown implements Runnable {
 		support.addPropertyChangeListener(listener);
 	}
 
+
+
 	@Override
 	public void run() {
-		Duration duration = Duration.between(endTime.toLocalDateTime(), localTimeIn1Hour);
+		Duration duration = Duration.between(endTime, localTimeIn1Hour);
 
-		System.out.println("itemID = " + itemID + " | " + "endtime = " + endTime);             //Skal fjernes senere
 
-		System.out.println(!duration.isNegative());
 		if (!(duration.isNegative())) {
-			System.out.println("Inside of if");
 			try {
 				Thread.sleep(duration.toMillis());
 			} catch (InterruptedException e) {
@@ -37,7 +37,6 @@ public class AuctionCountDown implements Runnable {
 			}
 		}
 
-		System.out.println("FIRING PROPERTY");
 		support.firePropertyChange("Time is up on item " + itemID, null, itemID);
 
 	}
---
+++ test/server/DatabaseAccessTest.java
@@ -0,0 +1,14 @@
+package server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseAccessTest {
+
+
+
+
+
+
+}
