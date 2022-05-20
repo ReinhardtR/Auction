eb043c3b65d7e7cf97890f585b6ba6d7732e03b0
Lemske <simon.sohn.privat@gmail.com>
2022-05-20 11:20:14 +0200

    SQLUtils update
    SQL vil nu altid repræsentere tables på vores database. Den fortæller også hvad der går galt og hvor, hvis man prøver at kalde et table/column der ikke eksistere på databasen.

--- test/server/persistence/utils/sqlcode/SQLStatementsTest.java
+++ test/server/persistence/utils/sqlcode/SQLStatementsTest.java
@@ -7,36 +7,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SQLStatementsTest {
 	SQLStatements statements = new SQLStatements();
-	Table test = new Table() {
-	};
+	Table test = new Table("testSchema", "test");
+
 
 	@Test
 	void selectTest() {
-		assertEquals("SELECT test, test FROM \"null\".null WHERE test = true", statements.select(test, new String[]{"test", "test"}, "test = true"));
+		assertEquals("SELECT test, test FROM \"testSchema\".test WHERE test = true", statements.select(test, new String[]{"test", "test"}, "test = true"));
 	}
 
 	@Test
 	void updatTest() {
-		assertEquals("UPDATE \"null\".null SET test = true WHERE test = false", statements.update(test, "test = true", "test = false"));
+		assertEquals("UPDATE \"testSchema\".test SET test = true WHERE test = false", statements.update(test, "test = true", "test = false"));
 	}
 
 	@Test
 	void testDelete() {
-		assertEquals("DELETE \"null\".null WHERE test = true", statements.delete(test, "test = true"));
+		assertEquals("DELETE \"testSchema\".test WHERE test = true", statements.delete(test, "test = true"));
 	}
 
 	@Test
 	void unionTest() {
-		assertEquals("SELECT test, test FROM \"null\".null WHERE test = true"
+		assertEquals("SELECT test, test FROM \"testSchema\".test WHERE test = true"
 						+ " UNION " +
-						"SELECT test, test FROM \"null\".null WHERE test = true", statements.union(new Table[]{test, test}, new String[]{"test", "test"}, "test = true"));
+						"SELECT test, test FROM \"testSchema\".test WHERE test = true", statements.union(new Table[]{test, test}, new String[]{"test", "test"}, "test = true"));
 	}
 
 	@Test
 	void coalesce() {
-		assertEquals("SELECT COALESCE(null.test1, null.test1) as test1, " +
-										"COALESCE(null.test2, null.test2) as test2, " +
-										"COALESCE(null.test3, null.test3) as test3 " +
+		assertEquals("SELECT COALESCE(test.test1, test.test1) as test1, " +
+										"COALESCE(test.test2, test.test2) as test2, " +
+										"COALESCE(test.test3, test.test3) as test3 " +
 										"FROM auction " +
 										"FULL JOIN buyout on auction.itemid = buyout.itemid " +
 										"ORDER BY test1 " +
