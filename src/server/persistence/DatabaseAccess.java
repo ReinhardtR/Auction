aed1b21a811bd32f75ac490fb1ff0939545d934f
Shmeat <miloamadsen@gmail.com>
2022-05-24 11:10:37 +0200

    Adder funktionalitet til database for at kunne adde til database

--- src/server/persistence/DatabaseAccess.java
+++ src/server/persistence/DatabaseAccess.java
@@ -1,29 +1,33 @@
 package server.persistence;
 
 import server.model.item.ItemImpl;
+import server.persistence.item.adderToDatabase.AdderToDatabase;
 import server.persistence.item.mutation.BuyingMutator;
 import server.persistence.utils.SQL;
 import server.persistence.utils.resultSetAdapter.ResultSetAdapter;
 import server.persistence.utils.resultSetAdapter.ResultSetAdapterImpl;
+import shared.SaleStrategyType;
 import shared.network.model.Item;
 
 import java.beans.PropertyChangeEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.List;
 
-public class DatabaseAccess implements DatabaseIO {
+public class DatabaseAccess implements CustomerDatabaseMethods, SalesManDatabaseMethods {
 	private final BuyingMutator buyingMutator;
 	private final long ONE_HOUR_IN_MILLI;
 	private final ResultSetAdapter resultSetAdapter;
+	private final AdderToDatabase adderToDatabase;
+
 
 	public DatabaseAccess() {
 		buyingMutator = new BuyingMutator();
 		ONE_HOUR_IN_MILLI = 3600000;
 		resultSetAdapter = new ResultSetAdapterImpl();
-
-		SQL.constructDatabaseTables(createConnection());
+		adderToDatabase = new AdderToDatabase();
 		//checkAuctionTimers();
 	}
 
@@ -94,5 +98,24 @@ public class DatabaseAccess implements DatabaseIO {
 	@Override
 	public synchronized void updateAuctionOffer(Item item) throws SQLException {
 		buyingMutator.newBid(createConnection(), (ItemImpl) item);
+	}
+
+	@Override
+	public void addItemToDatabase(Item itemToAdd) throws SQLException {
+		adderToDatabase.addItemToDatabase(createConnection(),(ItemImpl) itemToAdd);
+	}
+
+	@Override
+	public void AlterItemOnDatabsae(String itemIDToAlter, String columnToAlter, String newValue) throws SQLException {
+
+	}
+
+
+	public static void main(String[] args) {
+
+
+		DatabaseAccess access = new DatabaseAccess();
+		SQL.constructDatabaseTables(access.createConnection());
+		SQL.addAuctionItem(0, LocalDateTime.now(), SaleStrategyType.AUCTION);
 	}
 }
