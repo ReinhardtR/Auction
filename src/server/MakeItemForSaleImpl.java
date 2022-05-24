2ba9d713c1b176af9bcdb25e8fcf54a1019608d5
ymersod <simon.99@live.dk>
2022-05-24 13:17:15 +0200

    fixi
    fixi

--- src/server/MakeItemForSaleImpl.java
+++ src/server/MakeItemForSaleImpl.java
@@ -3,31 +3,43 @@ package server;
 import server.model.item.ItemImpl;
 import server.model.item.SaleStrategy.AuctionStrategy;
 import server.model.item.SaleStrategy.BuyoutStrategy;
+import server.persistence.DatabaseAccess;
+import server.persistence.SalesManDatabaseMethods;
 import shared.SaleStrategyType;
 import shared.network.model.Item;
 
 import java.rmi.RemoteException;
+import java.sql.SQLException;
 
 public class MakeItemForSaleImpl implements MakeItemForSale {
 
+	private final SalesManDatabaseMethods salesManDatabaseMethods;
+
 	public MakeItemForSaleImpl() {
+		salesManDatabaseMethods = new DatabaseAccess();
 	}
 
 	@Override
 	public void makeItem(String title, String description, String tags, SaleStrategyType saleType, String username, double offer, String endtime) {
 		try {
+			Item item = null;
 			if (saleType == SaleStrategyType.AUCTION) {
-				Item item = new ItemImpl(username, title, description, tags, new AuctionStrategy(offer, "", null));
+				item = new ItemImpl(username, title, description, tags, new AuctionStrategy(offer, "", null));
 			} else if (saleType == SaleStrategyType.BUYOUT) {
-				Item item = new ItemImpl(username, title, description, tags, new BuyoutStrategy(offer));
+				item = new ItemImpl(username, title, description, tags, new BuyoutStrategy(offer));
 			}
+			sendItemToDatabase(item);
 		} catch (RemoteException e) {
 			throw new RuntimeException();
 		}
 	}
 
-	private void sendItemToDatabase() {
-		System.out.println("DATA BASE KALD");
+	private void sendItemToDatabase(Item item) {
+		try {
+			salesManDatabaseMethods.addItemToDatabase(item);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 
