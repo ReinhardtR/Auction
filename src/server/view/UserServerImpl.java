5cf82cd341aebcbc73dcb3f5eee7922e3f562508
ymersod <simon.99@live.dk>
2022-05-31 20:10:57 +0200

    sdf
    sdf

--- src/server/view/UserServerImpl.java
+++ src/server/view/UserServerImpl.java
@@ -1,19 +1,19 @@
 package server.view;
 
 import shared.network.server.CustomerServer;
-import shared.network.server.MainServer;
+import shared.network.server.UserServer;
 import shared.network.server.SalesmanServer;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class MainServerImpl extends UnicastRemoteObject implements MainServer {
+public class UserServerImpl extends UnicastRemoteObject implements UserServer {
 
 	private final CustomerServer customerServer;
 	private final SalesmanServer salesmanServer;
 
 
-	public MainServerImpl(CustomerServerImpl customerServer, SalesmanServerImpl salesmanServer) throws RemoteException {
+	public UserServerImpl(CustomerServerImpl customerServer, SalesmanServerImpl salesmanServer) throws RemoteException {
 		this.customerServer = customerServer;
 		this.salesmanServer = salesmanServer;
 	}
--- src/shared/network/server/UserServer.java
+++ src/shared/network/server/UserServer.java
@@ -3,7 +3,7 @@ package shared.network.server;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface MainServer extends Remote {
+public interface UserServer extends Remote {
 
 	CustomerServer getCustomerServer() throws RemoteException;
 
